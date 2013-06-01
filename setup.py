3767bdb46d16529e3b1f6aa30c9a761100f61546
Joe Colosimo <jwc@jwcxz.com>
2013-06-01 00:23:26 -0400

    fixed bug with -t switch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import  setup
 
 setup(
     name="Pandemic",
-    version="1.0.2",
+    version="1.0.3",
     author="http://jwcxz.com/",
     url="http://jwcxz.com/projects/vim-pandemic",
     packages=["Pandemic"], 
