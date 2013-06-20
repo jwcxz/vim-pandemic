1d0a2c3a65bd32c4a2ef4eb3ba0d31b93affc3bc
Joe Colosimo <jwc@jwcxz.com>
2013-06-20 08:30:46 -0700

    setup.py now requires argparse

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import  setup
 
 setup(
     name="Pandemic",
-    version="1.1.1",
+    version="1.1.2",
     author="http://jwcxz.com/",
     url="http://jwcxz.com/projects/vim-pandemic",
+    requires=["argparse"],
     packages=["Pandemic"], 
     scripts=["bin/pandemic"]
 )
