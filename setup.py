13c2b5053d2e8c26e2cedc96c05dcf6f500909e4
Joe Colosimo <jwc@jwcxz.com>
2013-12-06 22:50:33 -0800

    sort bundle list to make it consistent

--- setup.py
+++ setup.py
@@ -2,14 +2,10 @@ from distutils.core import  setup
 
 setup(
     name="Pandemic",
-    version="1.1.4",
+    version="1.2.0",
     author="http://jwcxz.com/",
     url="http://jwcxz.com/projects/vim-pandemic",
     requires=["argparse"],
     packages=["Pandemic"], 
     scripts=["bin/pandemic"]
 )
-
-
-
-        
